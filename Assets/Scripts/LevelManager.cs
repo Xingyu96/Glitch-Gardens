ba8f17fc306cfb51a7f070948447b6e3ffda7087
r4i0b <taoxingyu@gmail.com>
2016-05-24 22:19:04 -0700

    added scripts, audio folders; modified LevelManager to autoload next level after 5 seconds on start;

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public float autoLoadNextLevelAfter = 5;
+
+    void Start()
+    {
+        Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+    }
+    public void loadLevel(string name){
+        //Application.LoadLevel(name);
+        SceneManager.LoadScene(name);
+
+    }
+
+    public void quitRequest()
+    {
+        Application.Quit();
+    }
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
