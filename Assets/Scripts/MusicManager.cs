6701c275f459527fa66ff83d5d75d0c1b644c0e9
r4i0b <taoxingyu@gmail.com>
2016-05-30 21:45:12 -0700

    "yes" "

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -4,13 +4,29 @@ using System.Collections;
 public class MusicManager : MonoBehaviour {
 
     public AudioClip[] levelMusicChange;
-	// Use this for initialization
-	void Start () {
-	
+
+    private AudioSource audiosource;
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    
+    
+    // Use this for initialization
+    void Start () {
+        audiosource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void OnLevelWasLoaded (int level) {
+        AudioClip thisLevelMusic = levelMusicChange[level];
+
+        if (thisLevelMusic)
+        {
+            audiosource.clip = thisLevelMusic;
+            audiosource.loop = true;
+            audiosource.Play();
+        }
 	
 	}
 }
